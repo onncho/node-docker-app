4d01b6c0358e624c247ac567ac8c229b361205a8
onncho <onncho@users.noreply.github.com>
2017-08-20 05:24:21 +0300

    connect postgres db

--- node/src/server.js
+++ node/src/server.js
@@ -11,6 +11,18 @@ let votes = {
     tacos: 0
 };
 
+let client = new pg.Client('postgres://postgres@172.17.0.1:9000/postgres');
+
+client.connect(function (err) {
+    if (err) throw err;
+
+    client.query('SELECT number_of_votes FROM votes', function (err, res) {
+        if (err) throw err;
+        votes.sandwitches = res.rows[0].number_of_votes;
+        votes.tacos = res.rows[1].number_of_votes;
+    });
+});
+
 let urlEncodedParser = bodyParser.urlencoded({
     extended: false
 });
@@ -28,8 +40,14 @@ app.post("/vote", urlEncodedParser, function (req, res) {
     let vote = req.body.yourVote;
     if (vote === 'sandwitches') {
         votes.sandwitches++;
+        client.query('UPDATE votes set number_of_votes=' + votes.sandwitches + 'WHERE option_name=\'sandwitches\'', function (err, result) {
+            if (err) throw err;
+        });
     } else if (vote === 'tacos') {
         votes.tacos++;
+        client.query('UPDATE votes set number_of_votes=' + votes.tacos + 'WHERE option_name=\'tacos\'', function (err, result) {
+            if (err) throw err;
+        });
     } else {
         console.log("Something went wrong..." + vote);
     }
---
+++ postgres/seed.sql
@@ -0,0 +1,7 @@
+create table votes (
+    id integer primary key, 
+    number_of_votes integer, 
+    option_name varchar(20)
+    );
+insert into votes (id, number_of_votes, option_name) values (1, 0, 'sandwitches');
+insert into votes (id, number_of_votes, option_name) values (2, 0, 'tacos');
